3aed121cfa9e8b30711df2c174ec367a53c445f5
Deke <Dekefective@gmail.com>
2022-02-05 19:31:21 +0000

    Palindrome solutions

---
+++ exercises/02-palindrome/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// Solution 1
+// function palindrome(string) {
+//   const reversed = string.split("").reverse().join("");
+//   return string === reversed;
+// }
+
+// Solution 2
+function palindrome(string) {
+  return string
+    .split("")
+    .every((char, i) => char === string[string.length - i - 1]);
+}
+
+module.exports = palindrome;
